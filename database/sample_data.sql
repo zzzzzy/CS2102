02fcc4c08c5fe30c818583165bfd99b5415cb4ac
shenlinnn <slinda1992@gmail.com>
2016-10-08 13:45:30 +0800

    add database

---
+++ database/sample_data.sql
@@ -0,0 +1,12 @@
+INSERT INTO USERS (user_name, email, phone, password, points, address, date_joined) VALUES (
+'admin','admin@gmail.com',11111111,'00000000',100,'PGPR','2016-01-01');
+INSERT INTO USERS (user_name, email, phone, password, points, address, date_joined) VALUES (
+'user','user@gmail.com',22222222,'00000000',100,'PGPR','2016-02-01');
+
+INSERT INTO PRODUCTS (title, description, owner_id, is_available) VALUES (
+'database management book','textbook for CS2102', 2, true);
+INSERT INTO PRODUCTS (title, description, owner_id, is_available) VALUES (
+'chair','spare chair', 2, true);
+
+
+
