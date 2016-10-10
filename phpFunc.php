aa0db9482261423eeffd12a97236a035d7af2dc3
Kim <kim@thecarousell.com>
2016-10-10 09:52:18 +0800

    My Bids and All Products Functions

--- phpFunc.php
+++ phpFunc.php
@@ -146,4 +146,92 @@ function addUserItem($item_title,$item_description,$item_availability){
 ## Destroy each session
 #session_destroy(); 
 
+################## My Bids ####################
+
+function retrieveBid($bid_id) {
+// User: Function to retrieve current user
+
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "cs2102";
+
+	$mysqli = new mysqli($host,$username,$password,$dbname);
+
+	$query = "SELECT * FROM BIDS WHERE bid_id ='".$bid_id."'";
+	$result = mysqli_query($mysqli, $query);
+	return $result;
+}
+
+function retrieveUserBids($user) {
+// User: Function to retrieve all items of the user
+
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "cs2102";
+
+	$mysqli = new mysqli($host,$username,$password,$dbname);
+	$query =  "SELECT * FROM BIDS b, USERS u WHERE b.bidder_id = u.user_id AND u.user_id= '".$user."'";
+	$result = mysqli_query($mysqli,$query);
+
+	return $result;
+}
+
+
+function deleteBid($bid_id) {
+		// Admin: Function to delete the camera
+
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "cs2102";
+
+	$mysqli = new mysqli($host,$username,$password,$dbname);
+
+	$query = "DELETE FROM BIDS WHERE bid_id=".$bid_id."";
+	mysqli_query($mysqli, $query);
+	$result = mysqli_affected_rows($mysqli);
+
+	return $result;
+}
+
+function addBids($auction_id, $bid_product_id, $bid_points, $bid_borrow_time,$bid_return_time,$bid_pickup){
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "cs2102";
+
+	$mysqli = new mysqli($host,$username,$password,$dbname);
+
+	$bid_bidderid = $_SESSION['user'];
+	$bid_time_created = date('Y-m-d H:i:s');
+
+	$query = "INSERT INTO BIDS (auctions, bidder_id, product_id, points, time_created, borrow_time, return_time, pickup) VALUES (?, $bid_bidderid, ?, ?, $bid_time_created, ?, ?, ?)";
+
+	$stmt = $mysqli->prepare($query);
+	$stmt->bind_param("iidsss", $auction_id, $bid_product_id, $bid_points, $bid_borrow_time,$bid_return_time,$bid_pickup);
+	$stmt->execute();
+	$stmt->close();
+	$result = $mysqli->affected_rows;
+	return $result;
+}
+
+################## All Products ####################
+
+function retrieveAvailProducts() {
+// User: Function to retrieve current user
+
+	$host = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "cs2102";
+
+	$mysqli = new mysqli($host,$username,$password,$dbname);
+
+	$query = "SELECT a.*, p.title FROM AUCTIONS a, PRODUCT p WHERE a.product_id = p.product_id and status ='open' ";
+	$result = mysqli_query($mysqli, $query);
+	return $result;
+}
+
 ?>
